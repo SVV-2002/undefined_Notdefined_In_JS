8fec8321016366b3c2649e20916ae558b278dbfd
SVV-2002 <90024570+SVV-2002@users.noreply.github.com>
2021-11-01 14:05:07 +0530

    Add files via upload

---
+++ undefined_Notdefined_In_JS.js
@@ -0,0 +1,10 @@
+var x;
+console.log("Declared a variable x but didn't assign a value to it yet...");
+console.log("x = "+x);
+x=10;
+console.log("Assigned a value to variable x now...");
+console.log("x = "+x);
+console.log("x is defined now...")
+console.log("Didn't declare a variable y...");
+console.log("y = ");
+console.log(y);
